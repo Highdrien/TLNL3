5830fec118b3ce62cfbcc37c8fc1f042a63e1428
Highdrien <adrien.zabban@outlook.fr>
2023-09-27 23:53:13 +0200

    add parameters

---
+++ parameters.py
@@ -0,0 +1,14 @@
+import os
+
+
+# Data param
+CONTEXT_LENGTH = 3
+FILE_TRAIN_1 = os.path.join('data', 'Le_comte_de_Monte_Cristo.train.100.unk5.tok'),
+
+# Model param
+EMBEDDING_DIM = 100
+HIDDEN_LAYER = 100
+
+# Training param
+NUM_EPOCHS = 10
+LEARNING_RATE = 0.1
