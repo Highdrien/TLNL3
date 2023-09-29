6c4ad96a40c7a3e059cdd3131f6964493ad407d9
Highdrien <adrien.zabban@outlook.fr>
2023-09-29 11:00:32 +0200

    add accuracy

--- train.py
+++ train.py
@@ -2,11 +2,11 @@ import torch
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import MultiStepLR
 from tqdm import tqdm
-import numpy as np
 
 from data import DataGenerator
 from model import Model
 import parameters as PARAM 
+from metrics import accuracy
 
 
 def train(save_weigth: bool) -> None:
@@ -55,10 +55,12 @@ def train(save_weigth: bool) -> None:
     optimizer = torch.optim.Adam(model.parameters(), lr=PARAM.LEARNING_RATE)
     scheduler = MultiStepLR(optimizer, milestones=PARAM.MILESSTONE, gamma=PARAM.GAMMA)
 
+    n_train, n_val = len(train_generator), len(val_generator)
 
     for epoch in range(1, PARAM.NUM_EPOCHS + 1):
         print('epoch:', epoch)
         train_loss = 0
+        train_acc = 0
         train_range = tqdm(train_generator)
 
         # Training
@@ -72,6 +74,8 @@ def train(save_weigth: bool) -> None:
             loss = criterion(y_pred, y_true)
 
             train_loss += loss.item()
+            y_pred = torch.nn.functional.softmax(y_pred, dim=1)  
+            train_acc += accuracy(y_true, y_pred)
 
             loss.backward()
             optimizer.step()
@@ -79,10 +83,14 @@ def train(save_weigth: bool) -> None:
 
             train_range.set_description("TRAIN -> epoch: %4d || loss: %4.4f" % (epoch, loss.item()))
             train_range.refresh()
+
+        print("TRAIN -> loss: %4.4f | acc: %4.2f" % (train_loss / n_train, train_acc / n_train))
+
         
         # Validation
         with torch.no_grad():
             val_loss = 0
+            val_acc = 0
             val_range = tqdm(val_generator)
             for x, y_true in val_range:
                 x.to(device)
@@ -91,15 +99,17 @@ def train(save_weigth: bool) -> None:
                 y_pred = model.forward(x)
 
                 loss = criterion(y_pred, y_true)
-
+                
                 val_loss += loss.item()
+                y_pred = torch.nn.functional.softmax(y_pred, dim=1)  
+                val_acc += accuracy(y_true, y_pred)
 
                 val_range.set_description("VAL -> epoch: %4d || loss: %4.4f" % (epoch, loss.item()))
                 val_range.refresh()
         
 
-        print("train loss: %4.4f | val loss: %4.4f" % (train_loss / len(train_generator), val_loss / len(val_generator)))
-    
+        print("VAL -> loss: %4.4f | acc: %4.2f" % (val_loss / n_val, val_acc / n_val))
+
         if save_weigth:
             model.save(PARAM.CHECKPOINT_PATH)
         scheduler.step()
